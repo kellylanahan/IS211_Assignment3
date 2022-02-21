dc2645a6a843f07cdb92b0605e775c1dd8051c38
kellylanahan <78877452+kellylanahan@users.noreply.github.com>
2022-02-20 20:05:33 -0500

    Add files via upload

--- assignment3.py
+++ assignment3.py
@@ -1,15 +1,47 @@
 import argparse
-# other imports go here
+import pandas as pd
+import re
 
 
-def main(url):
-    print(f"Running main with URL = {url}...")
+def image_format_func(text):
+    image_format = ['.jpg', '.png', '.gif']
+    for i in image_format:
+        if bool(re.search(i, text)) == True:
+            return True
+    return False
 
 
-if __name__ == "__main__":
-    """Main entry point"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
-    args = parser.parse_args()
-    main(args.url)
-    
+def browser_func(text):
+    if bool(re.search('Firefox', text)) == True:
+        return 'Mozilla Firefox'
+    elif bool(re.search('Chrome', text)) == True:
+        return 'Google Chrome'
+    elif bool(re.search('Explorer', text)) == True:
+        return 'Internet Explorer'
+    elif bool(re.search('Safari', text)) == True:
+        return 'Safari'
+
+
+def hour_func(text):
+    return list(text.split())[-1][:2]
+
+
+# PART II
+df = pd.read_csv('ENTER YOUR CSV FILE NAME HERE')
+
+
+# PART III
+df['image_bool'] = df['path_to_file'].apply(lambda x: image_format_func(x), axis=1)
+percentage = (len(df[df['image_bool'] == True]) / len(df['image_bool'])) * 100
+print('Image requests account for {} of all requests'.format(percentage))
+
+# PART IV
+df['browser'] = df['browser'].apply(lambda x: browser_func(x), axis=1)
+print('Browsers with maximum occurences: ', df['browser'].mode().to_list())
+
+# PART VI
+df['hour'] = df['datetime_accessed'].apply(lambda x: hour_func(x), axis=1)
+hour_dict = df['hour'].value_counts().to_dict()
+
+for hour, hits in hour_dict.items():
+    print('Hour {} has {} hits'.format(hour, hits))
